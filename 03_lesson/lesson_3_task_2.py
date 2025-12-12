92a93bbdc50f164978be9a99630664920237a4bd
imidg1825 <imidg18251972@gmail.com>
2025-12-13 02:05:31 +0300

    Lesson 3: OOP (User and Smartphone classes)

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,9 @@
+from smartphone import Smartphone
+
+phone1 = Smartphone("Apple", "iPhone 15", "+79990000001")
+phone2 = Smartphone("Samsung", "Galaxy S23", "+79990000002")
+phone3 = Smartphone("Xiaomi", "Redmi Note 12", "+79990000003")
+
+phone1.print_info()
+phone2.print_info()
+phone3.print_info()
---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,9 @@
+class Smartphone:
+    def __init__(self, brand, model, phone_number):
+        self.brand = brand
+        self.model = model
+        self.phone_number = phone_number
+
+    def print_info(self):
+       print(f"{self.brand} - {self.model}. {self.phone_number}")
+
